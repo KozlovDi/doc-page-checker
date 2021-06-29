f2c5ea2ca9daefd37ea6d108fc8d39ea52268296
evgenyKravchenk <pogifib937@geekale.com>
2021-06-29 23:57:53 +0600

    Add class Loader interface and pdf loader implementation

---
+++ src/main/java/com/it/one/practice/loaders/PdfLoader.java
@@ -0,0 +1,26 @@
+package com.it.one.practice.loaders;
+
+import com.it.one.practice.docs.Doc;
+import com.it.one.practice.docs.PdfDoc;
+import org.apache.pdfbox.pdmodel.PDDocument;
+
+import java.io.File;
+import java.io.IOException;
+
+public class PdfLoader implements DocLoader {
+
+    public PdfLoader() {
+
+    }
+
+    @Override
+    public Doc load(File file) {
+        PDDocument doc = null;
+        try {
+            doc = PDDocument.load(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new PdfDoc(doc);
+    }
+}
