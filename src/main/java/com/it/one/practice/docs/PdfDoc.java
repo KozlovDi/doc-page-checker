69c92c353d4ec9ecaf86feddafef00962ab2f2a2
evgenyKravchenk <pogifib937@geekale.com>
2021-07-05 16:56:43 +0600

    Add tests for PdfLoader

--- src/main/java/com/it/one/practice/docs/PdfDoc.java
+++ src/main/java/com/it/one/practice/docs/PdfDoc.java
@@ -5,6 +5,7 @@ import org.apache.pdfbox.rendering.PDFRenderer;
 
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.Objects;
 
 public class PdfDoc implements Doc{
 
@@ -23,5 +24,22 @@ public class PdfDoc implements Doc{
     public BufferedImage renderPage(int pageNumber) throws IOException {
         PDFRenderer pdfRenderer = new PDFRenderer(this.document);
         return pdfRenderer.renderImageWithDPI(pageNumber, 300);
+    }
+
+    @Override
+    public boolean equals(Object anotherDoc) {
+        if (this == anotherDoc) {
+            return true;
+        }
+        if (anotherDoc == null || getClass() != anotherDoc.getClass()) {
+            return false;
+        }
+        PdfDoc pdfDoc = (PdfDoc) anotherDoc;
+        return Objects.equals(document, pdfDoc.document);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(document);
     }
 }
